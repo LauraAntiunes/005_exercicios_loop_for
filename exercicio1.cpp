b69362e81cc1621c43e72c0153c6bf1963bf8d93
LauraAntiunes <101334124+LauraAntiunes@users.noreply.github.com>
2025-10-06 14:40:10 -0300

    Update exercicio1.cpp

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -7,26 +7,38 @@ a) Crie uma região paralela com #pragma omp parallel.
 
 b) Cada thread deve imprimir uma mensagem indicando seu número 
 (omp_get_thread_num()) e o total de threads (omp_get_num_threads()).
+
+
+c) Configure o programa para rodar com 4 threads.
 */
 
+
 #include <iostream> 
+#include <sstream>    // std::ostringstream (buffer local de string)
 #include <omp.h> 
 
 int main() {
  
-    #pragma omp parallel 
+    #pragma omp parallel num_threads(4) // cria um time de 4 threads
     {
         // Cada thread recebe um ID único de 0 a (T-1), onde T = número de threads.
-        int thread_id = omp_get_thread_num();
+        int tid = omp_get_thread_num();
+        int tot = omp_get_num_threads();
 
-        // Atenção: várias threads escrevendo no mesmo stdout podem "misturar" saídas.
-        // Para garantir a impressão linha-a-linha sem entrelaçamento, usar criticals. Próximo script.
-        std::cout << "Hello, world from thread " << thread_id << std::endl;
+        std::ostringstream oss;
+      
+
+        oss << "Hello, world from thread " << tid << " of " << tot << "\n";    
+    
+    
+        #pragma omp critical
+        {
+            std::cout << oss.str();
+        }
+    
+    
     }
     return 0;
 
 }
 
-/*
-c) Configure o programa para rodar com 4 threads.
-*/
