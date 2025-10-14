0b52680c37b215274c061dfb1bf1c7848dbc26e6
LauraAntiunes <101334124+LauraAntiunes@users.noreply.github.com>
2025-10-14 19:09:34 -0300

    Update exercicio2.cpp

--- exercicio2.cpp
+++ exercicio2.cpp
@@ -1,25 +1,32 @@
+/* Exercício 2 — Paralelizando um for simples
+a) Crie um vetor v de tamanho 100 e inicialize todos os elementos com o valor 1.
+b) Escreva um loop sequencial que soma todos os elementos.
+c) Refaça o loop com #pragma omp parallel for reduction(+:soma).
+d) Compare os resultados e explique por que a diretiva reduction é necessária. */
+
+
 #include <iostream>
 #include <vector>
 #include <omp.h>
 
 int main() {
     const int N = 100;
-    std::vector<int> v(N, 1); // a) vetor com 100 elementos valendo 1
+    std::vector<int> v(N, 1); // a) 
 
-    // b) Soma sequencial
+    // b) 
     int soma_seq = 0;
     for (int i = 0; i < N; ++i) {
         soma_seq += v[i];
     }
 
-    // c) Soma paralela com reduction
+    // c) 
     int soma_par = 0;
     #pragma omp parallel for reduction(+:soma_par)
     for (int i = 0; i < N; ++i) {
         soma_par += v[i];
     }
 
-    // d) Exibir resultados
+    // d) 
     std::cout << "Soma sequencial: " << soma_seq << std::endl;
     std::cout << "Soma paralela:   " << soma_par << std::endl;
 
